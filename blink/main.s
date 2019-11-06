333cb0e291684ecdbed3c9a8af2041fb187df201
Przemyslaw Kobylanski <przemko@mac.com>
2019-11-06 16:39:05 +0100

    poprawki

--- blink/main.s
+++ blink/main.s
@@ -1,23 +1,26 @@
+DDRB = 0x04
+PINB = 0x05
+	
 .org 0
-	sbi 0x4,0
+	sbi	DDRB,0
 	
 loop:
-	sbi 0x5,0
-	rcall delay		
-	cbi 0x5,0
-	rcall delay
-	rjmp loop
+	sbi	PINB,0
+	rcall	delay		
+	cbi	PINB,0
+	rcall	delay
+	rjmp	loop
 
 delay:
-	ldi r18,6
-	ldi r19,19
-	ldi r20,174
+	ldi	r18,6
+	ldi	r19,19
+	ldi	r20,174
 L1:
-	dec r20
-	brne L1
-	dec r19
-	brne L1
-	dec r18
-	brne L1
+	dec	r20
+	brne	L1
+	dec	r19
+	brne	L1
+	dec	r18
+	brne	L1
 	ret
 	
