1069e37f63fbaaa3ae6d1d39519bc850c089c7f8
Przemyslaw Kobylanski <przemko@mac.com>
2020-11-18 06:15:22 +0100

    dodano przykład LFSR

---
+++ lfsr8/main.s
@@ -0,0 +1,53 @@
+	;; Linear-feedback shift register
+	;;	
+	;; Loop cycle: 3.125 (lo) and 3.25 (hi) microseconds
+
+DDRB  = 0x04
+PINB  = 0x05
+SREG  = 0x3F
+TMP   = 0x00
+CLKPR = 0x61
+	
+.org 0
+	ldi	r18,0
+	ldi	r24,0x80
+	in	TMP,SREG 
+	cli
+	sts	CLKPR,r24
+	sts	CLKPR,r18
+	out	SREG,TMP
+
+	sbi	DDRB,0
+	eor	r0,r0
+	
+loop:
+	mov	r16,r0
+	andi	r16,0x01
+	mov	r17,r0
+	lsr	r0
+	eor	r18,r18
+	lsl	r17
+	lsl	r17
+	lsl	r17
+	eor	r18,r17
+	lsl	r17
+	eor 	r18,r17
+	lsl	r17
+	eor	r18,r17
+	lsl	r17
+	lsl	r17
+	eor	r18,r17
+	com	r18
+	bst	r18,7
+	bld	r0,7
+	tst	r16
+	breq	clear
+
+set:	
+	sbi	PINB,0
+	rjmp	loop
+
+clear:	
+	cbi	PINB,0
+	rjmp	loop
+
